bbfc56d474d6ae8362a5a9a3313b99d729f1e5a6
Kayro Mizael <kayromsa@gmail.com>
2020-02-19 23:33:32 -0300

    finish painel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import baseService from './core/baseService';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function getDevs() {
+      const response = await baseService.get('/devs');
+      setDevs(response.data);
+    }
+    getDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await baseService.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
-    <h1>Hellow World</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev}/>
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
 
